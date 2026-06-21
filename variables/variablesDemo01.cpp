8b725ec527dc3007ec4ce9cc1179c1ba65eb8f0f
鹰Al-Haitam <study.haitham@gmail.com>
2026-06-21 11:16:31 +0100

    new Lines

--- variables/variablesDemo01.cpp
+++ variables/variablesDemo01.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
-#include <string> 
+#include <string>
 
-int age = 23;
-string name = "Haitam";
-bool isOnline = true;
-char score = 'A';
+int main(){
+    int age;
+    string name;
+    bool isOnline = true;
+    string isON="OFF";
+    char score;
+    string isExit;
+    cout << "Your age: ";
+    cin >> age;
+    cout << "Your name: ";
+    cin >> name;
+    cout << "Your score: ";
+    cin >> score;
+    if (isOnline==true){
+        isON="ON";
+    };
+    cout << "User Status: " << isON << "\nYou're " << name << ", and you're " << age << " years old.\n Your score is " << score <<" !\n";
+    cout << "Exit? :";
+    cin >> isExit;
+    if (isExit=="yes"){
+        isON="OFF";
+    };
+}
